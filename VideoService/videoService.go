4b84a450f4cf81b3c7d9e88fe19ea2603c198b38
Jyotik Mayur <heyjyotik@gmail.com>
2023-07-26 08:54:57 +0100

    Working on CreateVideo method, currently storing the video and thumbnail locally, need to store them in cloud blob store

--- VideoService/videoService.go
+++ VideoService/videoService.go
@@ -1,7 +1,9 @@
 package video_service
 
 import (
+	"bytes"
 	"context"
+	"io"
 
 	"github.com/hashicorp/go-hclog"
 	"github.com/jyotikmayur7/YouCreo/api"
@@ -20,6 +22,49 @@ func NewVideoService(l hclog.Logger, db *database.DatabaseAccessor) *VideoServic
 	}
 }
 func (vs *VideoService) CreateVideo(stream api.VideoService_CreateVideoServer) error {
+
+	req, err := stream.Recv()
+	if err != nil {
+		vs.log.Error("Error: Unable to receive video info ", err)
+		return err
+	}
+
+	videoTitle := req.GetVideoTitle()
+	videoDescription := req.GetVideoDescription()
+
+	videoData := bytes.Buffer{}
+	videoThumbnail := bytes.Buffer{}
+
+	for {
+		vs.log.Info("Receiving video data")
+
+		req, err := stream.Recv()
+		if err == io.EOF {
+			vs.log.Info("Client has stopped the upload, upload is finished ", err)
+			break
+		}
+		if err != nil {
+			vs.log.Error("Error: Unable to read video chunk from client ", err)
+			return err
+		}
+
+		thumbnailChunk := req.GetVideoThumbnail()
+		if thumbnailChunk != nil {
+			videoThumbnail.Write(thumbnailChunk)
+		}
+
+		videoChunk := req.GetVideoContent()
+		// Instrading of storing data locally need to send this chunk to aws S3 to store it so that bytes.Buffer{} won't exceed the size limit
+		videoData.Write(videoChunk)
+	}
+
+	//TODO's
+
+	// AWS s3 store video call
+	//blobReference :=
+
+	//Store the info on database
+
 	return nil
 }
 
