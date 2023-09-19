f075f2334e23590034b5a22f07ef500cbe3d77c5
Jyotik Mayur <heyjyotik@gmail.com>
2023-09-19 09:16:51 +0100

    Tested the middleware, small refactor

--- middleware/context.go
+++ middleware/context.go
@@ -3,27 +3,26 @@ package middleware
 import (
 	"context"
 
-	"github.com/hashicorp/go-hclog"
 	"google.golang.org/grpc"
 )
 
 func AddContextInterceptorUnary(ctxMain context.Context) grpc.UnaryServerInterceptor {
 	return func(ctxSubMain context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
-		log := hclog.Default()
-		log.Error("Test Middleware from unary")
-		test := "testStringFromMiddleware"
-		ctx := context.WithValue(ctxMain, "test", test)
-		return handler(ctx, req)
+		// log := hclog.Default()
+		// log.Error("Test Middleware from unary")
+		// test := "testStringFromMiddleware"
+		// ctx := context.WithValue(ctxMain, "test", test)
+		return handler(ctxMain, req)
 	}
 }
 
 func AddContextInterceptorStream(ctxMain context.Context) grpc.StreamServerInterceptor {
 	return func(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
-		log := hclog.Default()
-		log.Error("Test Middleware from stream")
-		test := "testStringFromMiddleware"
-		ctx := context.WithValue(ctxMain, "test", test)
-		ss.Context().Value(ctx)
+		// log := hclog.Default()
+		// log.Error("Test Middleware from stream")
+		// test := "testStringFromMiddleware"
+		// ctx := context.WithValue(ctxMain, "test", test)
+		ss.Context().Value(ctxMain)
 		return handler(srv, ss)
 	}
 }
